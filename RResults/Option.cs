5e464d2979da0724f7b65c6f3f0f00d5f115b322
jakub <kubat260@gmail.com>
2025-11-22 23:39:49 +0100

    feat: added Option<T> type

---
+++ RResults/Option.cs
@@ -0,0 +1,45 @@
+﻿namespace RResults;
+
+/// <summary>
+/// Represents an optional value: every <see cref="Option{T}"/> is either <c>Some</c> and contains a value,
+/// or <c>None</c> and contains no value.
+/// </summary>
+/// <typeparam name="T">The type of the contained value.</typeparam>
+public struct Option<T>
+{
+    internal T _value;
+    internal readonly bool _isSome;
+    
+    /// <summary>
+    /// Creates an <see cref="Option{T}"/> that contains a value.
+    /// This represents the <c>Some</c> variant, meaning the option has a value.
+    /// </summary>
+    /// <param name="value">The value to wrap in the option.</param>
+    /// <returns>An <see cref="Option{T}"/> containing the given value.</returns>
+    public static Option<T> Some(T value) => new(value);
+    
+    /// <summary>
+    /// Returns an <see cref="Option{T}"/> representing the <c>None</c> variant,
+    /// meaning the option has no value.
+    /// </summary>
+    /// <returns>An <see cref="Option{T}"/> with no value.</returns>
+    public static Option<T> None => default;
+    
+    internal Option(T value)
+    {
+        _value = value;
+        _isSome = true;
+    }
+
+    /// <summary>
+    /// Returns <c>true</c> if the option contains a value (i.e., is the <c>Some</c> variant).
+    /// </summary>
+    /// <returns><c>true</c> if this option has a value; otherwise, <c>false</c>.</returns>
+    public bool IsSome() => _isSome;
+    
+    /// <summary>
+    /// Returns <c>true</c> if the option contains a value (i.e., is the <c>Some</c> variant).
+    /// </summary>
+    /// <returns><c>true</c> if this option has a value; otherwise, <c>false</c>.</returns>
+    public bool IsNone() => !_isSome;
+}
